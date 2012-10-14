49d60ed597d21bd7ab1ba24d11b3670dac02b843
Kevin Liddle <kevin@8thlight.com>
2012-10-14 01:28:23 -0500

    hide finished questions

--- public/javascripts/game-board.js
+++ public/javascripts/game-board.js
@@ -2,29 +2,44 @@ namespace("Jeopardy", {
   GameBoard: Backbone.View.extend({
 
     initialize: function() {
+      this.setTemplateInterpolator();
       this.boardInfo = this.options.boardInfo;
-      _.templateSettings = {
-        interpolate : /\{\{(.+?)\}\}/g
-      };
+      this.finishedQuestions = [];
+      this.boardTemplate = _.template($('#board-template').html());
+      this.clueTemplate = _.template($("#clue-template").html());
     },
 
     events: {
-      'click .clue-value' : 'showClue'
+      'click .clue-value' : 'showClue',
+      'click .clue'       : 'showBoard'
     },
 
-    render: function() {
-      var template = _.template($('#board-template').html());
-      $(this.el).html(template(this.boardInfo));
-      return this;
+    showBoard: function() {
+      $(this.el).html(this.boardTemplate(this.boardInfo));
+      this.hideFinishedQuestions();
     },
 
     showClue: function(event) {
       var target = $(event.currentTarget);
       var categoryNumber = target.data('category');
       var clueNumber = target.data('clue');
-      var template = _.template($("#clue-template").html());
 
-      $(this.el).html(template({clue: this.boardInfo[categoryNumber][clueNumber]}));
+      this.finishedQuestions.push(target.data());
+      $(this.el).html(this.clueTemplate({clue: this.boardInfo[categoryNumber][clueNumber]}));
+    },
+
+    setTemplateInterpolator: function() {
+      _.templateSettings = {
+        interpolate : /\{\{(.+?)\}\}/g
+      };
+    },
+
+    hideFinishedQuestions: function() {
+      _.each(this.finishedQuestions, function(question) {
+        var selector = '.clue-value[data-category="' + question.category +
+          '"][data-clue="' + question.clue + '"]';
+        $(selector).addClass('hidden');
+      });
     }
 
   })
--- spec/javascripts/game-board-spec.js
+++ spec/javascripts/game-board-spec.js
@@ -1,34 +1,52 @@
 describe("Jeopardy.GameBoard", function() {
 
-  var boardInfo = {some: "thing", category1: {clue1: "blibbety"}};
-  var board = function() {
-    return new Jeopardy.GameBoard({boardInfo: boardInfo, el: "#test-div"});
-  }
-
-  it("sets the boardInfo", function() {
-    expect(board().boardInfo).toEqual(boardInfo);
-  });
-
-  it("renders the board", function() {
-    setFixtures('<div id="board-template">{{ some }}</div><div id="test-div"></div>');
-    board().render();
-    expect($('#test-div').text()).toEqual(boardInfo["some"]);
-  });
-
-  it("calls 'showClue' when .clue-value is clicked", function() {
-    expect(board().events['click .clue-value']).toEqual('showClue');
-  });
-
-  it("shows the clue that is clicked", function() {
+  beforeEach(function() {
     setFixtures(
       '<div id="test-div"></div>' +
-        '<div id="target" class="clue-value" data-category="category1" data-clue="clue1"></div>' +
-        '<script type="text/template" id="board-template"><div>{{ some }}</div></script>' +
+        '<script type="text/template" id="board-template">' +
+        '<div id="target" class="clue-value" data-category="category1" data-clue="clue1">' +
+        '{{ some }}</div></script>' +
         '<script type="text/template" id="clue-template"><div class="clue">{{ clue }}</div></script>'
     );
-    var event = {currentTarget: '#target'}
-    board().showClue(event);
-    expect($('#test-div').text()).toEqual(boardInfo.category1.clue1);
+    this.boardInfo = {some: "thing", category1: {clue1: "blibbety"}};
+    this.board =  new Jeopardy.GameBoard({boardInfo: this.boardInfo, el: "#test-div"});
+  });
+
+  it("sets the boardInfo", function() {
+    expect(this.board.boardInfo).toEqual(this.boardInfo);
+  });
+
+  describe("events", function() {
+    it("calls 'showClue' when .clue-value is clicked", function() {
+      expect(this.board.events['click .clue-value']).toEqual('showClue');
+    });
+
+    it("calls 'showBoard' when .clue is clicked", function() {
+      expect(this.board.events['click .clue']).toEqual('showBoard');
+    });
+  });
+
+  describe("#showBoard", function() {
+    it("renders the board", function() {
+      this.board.showBoard();
+      expect($('#test-div').text()).toEqual(this.boardInfo["some"]);
+    });
+
+    it("hides a clue value after clicking on it", function() {
+      this.board.showBoard();
+      $('.clue-value').click();
+      $('.clue').click();
+      expect($('[data-category="category1"][data-clue="clue1"]')).toHaveClass("hidden");
+    });
+  });
+
+  describe("#showClue", function() {
+    it("shows the clue that corresponds to the value that is clicked", function() {
+      var event = {currentTarget: '#target'}
+      this.board.showBoard();
+      this.board.showClue(event);
+      expect($('#test-div').text()).toEqual(this.boardInfo.category1.clue1);
+    });
   });
 
 });
