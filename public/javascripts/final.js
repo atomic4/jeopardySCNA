c90a82f276e0b80ec37aff974bd33c189e414406
Kevin Liddle <kevin@8thlight.com>
2013-11-09 12:00:05 -0600

    final

--- public/javascripts/final.js
+++ public/javascripts/final.js
@@ -1,6 +1,6 @@
 namespace("Config", {
   Final: {
-    category: "",
-    clue: ""
+    category: "Text Editors",
+    clue: "Vim is an open source, freely distributable editor, but users are urged to donate money to help poor children in this nation"
   }
 });
